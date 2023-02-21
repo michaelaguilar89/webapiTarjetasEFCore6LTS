d6f97490386ad59a0a8036cfe20a31f18232ebee
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-02-20 22:06:23 -0600

    Using dependency injection I add the connection string that is in my app.settings.json

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiTarjetas.Conexion;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Using dependency injection I add the connection string that is in my app.settings.json
+
+builder.Services.AddDbContext<AppDBContext>(opt =>
+{
+	opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConection"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
