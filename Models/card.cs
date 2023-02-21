de6072f8d6a081eb6dfd0f3fc3d0841304a052a4
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-02-20 21:52:07 -0600

    aading card Model

---
+++ Models/card.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApiTarjetas.Models
+{
+	public class card
+	{
+		public int Id { get; set; }
+
+		public string userName { get; set; }
+
+		public string cardNUmber { get; set; }
+
+		public string ExpirationDate { get; set; }
+	}
+}
