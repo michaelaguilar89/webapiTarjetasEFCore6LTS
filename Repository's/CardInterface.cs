e9879cb8013b9d42efc55eb01733a6c1d25f0df9
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-02-21 00:00:19 -0600

    updating cardInterface

--- Repository's/CardInterface.cs
+++ Repository's/CardInterface.cs
@@ -1,6 +1,15 @@
-﻿namespace WebApiTarjetas.Repository_s
+﻿using WebApiTarjetas.Models;
+
+namespace WebApiTarjetas.Repository_s
 {
 	public interface CardInterface
 	{
+		Task<List<card>> GetCards();
+
+		Task<card> GetCardById(int id);
+
+		Task<string> createUpdateCard(card);
+
+		Task<string> removeCard(int id);
 	}
 }
