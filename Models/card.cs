a4612e2308406fe60427940c64e0f879fb41990d
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-02-20 21:56:52 -0600

    adding dataAnnotations on model

--- Models/card.cs
+++ Models/card.cs
@@ -1,13 +1,16 @@
-﻿namespace WebApiTarjetas.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiTarjetas.Models
 {
 	public class card
 	{
+		[Key]
 		public int Id { get; set; }
-
+		[Required]
 		public string userName { get; set; }
-
+		[Required]
 		public string cardNUmber { get; set; }
-
+		[Required]
 		public string ExpirationDate { get; set; }
 	}
 }
