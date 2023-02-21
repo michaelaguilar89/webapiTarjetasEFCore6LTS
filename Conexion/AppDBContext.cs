2b1dc721e203a33bdbaaef06b9032e6f13c2cf21
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-02-20 22:02:14 -0600

    adding dbContext

---
+++ Conexion/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiTarjetas.Models;
+
+namespace WebApiTarjetas.Conexion
+{
+	public class AppDBContext : DbContext
+	{
+		public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+		{
+		}
+		public DbSet<card> cards { get; set; }
+	}
+}
