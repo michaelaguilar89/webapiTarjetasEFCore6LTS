4ccfa94a54955a48da9b77b28e72ffe55ef7411e
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-02-22 11:08:52 -0600

    adding config cors

--- Program.cs
+++ Program.cs
@@ -2,7 +2,19 @@ using Microsoft.EntityFrameworkCore;
 using WebApiTarjetas.Conexion;
 using WebApiTarjetas.Repository_s;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy(name: MyAllowSpecificOrigins,
+					  policy =>
+					  {
+						  policy.WithOrigins("http://localhost:4200",
+											  "http://www.contoso.com");
+					  });
+});
+
 
 // Add services to the container.
 
@@ -28,7 +40,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors(MyAllowSpecificOrigins);
 app.UseAuthorization();
 
 app.MapControllers();
