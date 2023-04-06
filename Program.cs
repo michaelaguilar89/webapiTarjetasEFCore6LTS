82ef856a3fd206bc3b8b25f504965a18daf95cda
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-04-05 20:04:45 -0600

    publish on smartaspnet

--- Program.cs
+++ Program.cs
@@ -28,17 +28,18 @@ builder.Services.AddScoped<CardInterface,CardRepository>();
 
 builder.Services.AddDbContext<AppDBContext>(opt =>
 {
-	opt.UseSqlServer(builder.Configuration.GetConnectionString("SomeeConnection"));
+	opt.UseSqlServer(builder.Configuration.GetConnectionString("smartaspConnection"));
 });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
-
+//if (app.Environment.IsDevelopment())
+//{
+	//app.UseSwagger();
+	//app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseCors(MyAllowSpecificOrigins);
 app.UseAuthorization();
